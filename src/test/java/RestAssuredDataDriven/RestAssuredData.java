5f14daa423888247afd89e225cbb438c7fca82b7
divyashhura <97031108+divyashhura@users.noreply.github.com>
2022-02-17 01:09:15 +0530

    Post data from Excel sheet to StripeAPI

--- src/test/java/RestAssuredDataDriven/RestAssuredData.java
+++ src/test/java/RestAssuredDataDriven/RestAssuredData.java
@@ -1,45 +1,33 @@
 package RestAssuredDataDriven;
 
 import io.restassured.response.Response;
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Properties;
 import static io.restassured.RestAssured.given;
 
 public class RestAssuredData {
     public static String url="https://api.stripe.com/v1/customers";
-    static Properties prop;
     static String key="sk_test_51KTidySI0qvV4HQF1uPjvXbR8SvosKzGMYfcUKmYpyHoYzRl4trnNUESIyLfixltCTQHTMh1ehjZbASKWvu9tG1s000jTufouw";
 
-    @DataProvider(name="data")
-    public Object[][] ApiData() throws IOException {
-        Object[][] data=new Object[1][];
-        String path = System.getProperty("user.dir") + prop.getProperty("Path");
+    @Test
+    public void ApiData() throws IOException {
+        String path = System.getProperty("user.dir") + "/Files/UserDataApi.xlsx";
         System.out.println(path);
         FileInputStream fis = new FileInputStream(path);
         XSSFWorkbook wb = new XSSFWorkbook(fis);
         XSSFSheet sh = wb.getSheetAt(0);
-        for (int i = 1; i <= sh.getLastRowNum(); i++) {
-            data[i][0] = sh.getRow(i).getCell(0).getStringCellValue();
-            data[i][1] = sh.getRow(i).getCell(1).getStringCellValue();
-            data[i][2]=sh.getRow(i).getCell(2).getStringCellValue();
-        }
-        return data;
-    }
-    @Test(dataProvider = "data")
-    public void postdata(String email,String name,String phone){
+        String email = sh.getRow(1).getCell(0).getStringCellValue();
+        String name = sh.getRow(1).getCell(1).getStringCellValue();
+        String phone =sh.getRow(1).getCell(2).getStringCellValue();
         Response resp = given().auth().basic(key, "").formParam("email", email).
                 formParam("name", name).formParam("phone",phone).when().post(url);
+
         int respcode = resp.getStatusCode();
-        System.out.println("Responce code is: " + respcode);
+        System.out.println("Response code is: " + respcode);
+
         String respbody = resp.getBody().asString();
         System.out.println(respbody);
     }
